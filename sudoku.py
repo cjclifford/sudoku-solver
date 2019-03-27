17a7b35ac393aabf5fd4c577417d6217a8e0b9cf
Cuan Clifford <cuanc@bbd.co.za>
2019-03-27 17:47:44 +0200

    now takes files as input from command line

--- sudoku.py
+++ sudoku.py
@@ -1,49 +1,49 @@
+import fileinput
+
 def nextEmptyCell(grid):
-    for x in range(9):
-        for y in range(9):
-            if grid[x][y] == 0:
-                return x, y
-    return -1, -1
+	for x in range(9):
+		for y in range(9):
+			if grid[x][y] == 0:
+				return x, y
+	return -1, -1
 
 def isValid(grid, i, j, e):
-    if all([e != grid[x][j] for x in range(9)]):
-        if all([e != grid[i][y] for y in range(9)]):
-            secTopX, secTopY = 3 * (i // 3), 3 * (j // 3)
-            for x in range(secTopX, secTopX + 3):
-                for y in range(secTopY, secTopY + 3):
-                    if grid[x][y] == e:
-                        return False
-            return True
-        return False
+	if all([e != grid[x][j] for x in range(9)]):
+		if all([e != grid[i][y] for y in range(9)]):
+			secTopX, secTopY = 3 * (i // 3), 3 * (j // 3)
+			for x in range(secTopX, secTopX + 3):
+				for y in range(secTopY, secTopY + 3):
+					if grid[x][y] == e:
+						return False
+			return True
+	return False
 
 def solveSudoku(grid, i = 0, j = 0):
-    i, j = nextEmptyCell(grid)
-    if i == -1:
-        return True
+	i, j = nextEmptyCell(grid)
+	if i == -1:
+		return True
 
-    for e in range(1, 10):
-        if isValid(grid, i, j, e):
-            grid[i][j] = e
-            if solveSudoku(grid, i, j):
-                return True
-    grid[i][j] = 0
-    return False
+	for e in range(1, 10):
+		if isValid(grid, i, j, e):
+			grid[i][j] = e
+			if solveSudoku(grid, i, j):
+				return True
+	grid[i][j] = 0
+	return False
 
 def printSudoku(grid):
-    solveSudoku(grid)
-    for row in grid:
-        print(row)
+	f = open("easy.puz", "r")
+	print(f.read())
+	return
+	solveSudoku(grid)
+	for row in grid:
+		print(row)
+	print()
 
-test = [
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0]
-]
+def fileTest():
+	grid = []
+	for line in fileinput.input():
+		# grid.add(line.split(','))
+		print(line[:-1].split(','))
 
-printSudoku(test)
+fileTest()
